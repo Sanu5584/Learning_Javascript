130ab1f043d3cce5ce32b7c276bb6054ed96eff9
Sanu5584 <155883598+Sanu5584@users.noreply.github.com>
2024-01-26 10:57:37 +0000

    conversion of data types

---
+++ Sanidhya_js/Conversion_operator.js
@@ -0,0 +1,32 @@
+// let score = 33;   // it is a number 
+// let score = "33bac";   // it is a string 
+// let score = undefined;   // it is a NaN(not a number)
+// let score = null;   // it gives 0 
+// let score = true;   // it gives 1 (because it is a boolean)
+// let score = false;   // it gives 0 (because it is a boolean)
+let score = "Sanidhya";   // it gives NaN
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let ValueInNumber = Number(score);
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+
+let IsLoggedIn = 1;
+// 1 => true
+// 0 => false 
+// ""(Empty string) => false 
+// "Sanidhya" => true 
+ 
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
