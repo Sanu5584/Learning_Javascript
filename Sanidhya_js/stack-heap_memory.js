2187eb4e83c63b0a1f4414f11fdc90e4a823aec0
Sanu5584 <155883598+Sanu5584@users.noreply.github.com>
2024-01-29 11:49:33 +0000

    done

---
+++ Sanidhya_js/stack-heap_memory.js
@@ -0,0 +1,23 @@
+// Stack(Primitive)  &  Heap(Non-Primitive)
+
+let myYoutubeName = "hiteshchaudharydotcom";
+
+let anotherName = myYoutubeName;
+
+anotherName = "chaiaurcode";
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "hitesh@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
