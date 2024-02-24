9d17188134bf6ca5f1f5af8078826dddbe990ce8
Sanu5584 <155883598+Sanu5584@users.noreply.github.com>
2024-02-24 05:41:11 +0000

    done

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -1,9 +1,15 @@
 // singleton
+// object.create
+
 
 // objects literals
+
+const mySym = Symbol("key1");
+
 const JsUser = {
     name : "Sanidhya",
     "full name" : "Delvadiya Sanidhya",
+    [mySym] : "mykey",
     age : 18,
     location : "Ahmedabad",
     email : "sanu5584@gmail.com",
@@ -15,4 +21,19 @@ console.log(JsUser.email);
 console.log(JsUser["email"]);
 console.log(JsUser["full name"]);
 // console.log(JsUser.full name); // shows error which is why above square bracket method is imp
+console.log(JsUser[mySym]);
 
+JsUser.email = "Sanu5584@chatgpt.com";
+// Object.freeze(JsUser);
+JsUser.email = "Sanu5584@icrosoft.com";
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello Js Users");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js Users, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
