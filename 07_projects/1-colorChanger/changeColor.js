9570cf14e7714a9c0a2d7b846c910dac9b4b692c
Sanu5584 <155883598+Sanu5584@users.noreply.github.com>
2024-03-15 12:43:23 +0000

    done

---
+++ 07_projects/1-colorChanger/changeColor.js
@@ -0,0 +1,40 @@
+
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach((button) => {
+  console.log(button);
+
+  button.addEventListener('click', function(e){
+    console.log(e)
+    console.log(e.target)
+    if(e.target.id === "grey"){
+      body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id === "white"){
+      body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id === "blue"){
+      body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id === "yellow"){
+      body.style.backgroundColor = e.target.id
+    }
+  })
+});
+
+
+// method 2 
+
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector('body');
+
+// buttons.forEach((button) => {
+//   console.log(button);
+
+//   button.addEventListener('click', function(e){
+//     console.log(e)
+//     console.log(e.target)
+//       body.style.backgroundColor = e.target.id
+//   })
+// });
