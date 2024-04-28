11e267004c8643b41f452bf3ec6ae8114357a16f
Sanu5584 <155883598+Sanu5584@users.noreply.github.com>
2024-04-28 14:26:54 +0000

    done

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -56,3 +56,75 @@ promiseFour
 .finally(()=> {
     console.log("The promise is either resolved or rejected")
 });
+
+
+const promiseFive = new Promise((resolve,reject) => {
+    setTimeout(function(){
+        let car_payment = true;
+        if(car_payment == true){
+            resolve({
+                car_name : "Mahindra XUV700",
+                car_price : "30,00,000(onroad_Mumbai)",
+                car_model : "2024",
+                car_buyer: 'Sanidhya Delvadiya',
+                car_payment_type: "EMI",
+                car_payment: 'done'
+            });
+        }else{
+            reject("car payment not done");
+        }
+    },5000)
+})
+
+// promiseFive.then(function(car_detail){
+//     // console.log(car_detail);
+//     return car_detail.car_buyer;
+// })
+// .then(function(car_buyername){
+//     console.log(car_buyername);
+// })
+// .catch(function(err){
+//     console.log(err);
+// })
+// .finally(function(){
+//     console.log("Here are details about car payment done or not. Your result is given above");
+// })
+
+
+// using async await and try-catch
+// promise is an obj
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive;
+    console.log(response.car_price);
+    console.log(response.car_buyer);
+    console.log(response.car_model);
+    console.log(response.car_payment_type);
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+ 
+consumePromiseFive();
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log('E: ',error);
+//     }
+// } getAllUsers();
+
+fetch("https://api.github.com/users/hiteshchoudhary")
+.then((response) => {
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
