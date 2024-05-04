acda9d8587dcb22828454775885f25ba1fde6857
Sanu5584 <155883598+Sanu5584@users.noreply.github.com>
2024-05-04 05:58:20 +0000

    done

--- 10_classes_and_oops/13_object_get_set.js
+++ 10_classes_and_oops/13_object_get_set.js
@@ -1,1 +1,15 @@
-const User 
+const User = {
+    _email : "s@gmail.com",
+    _password : "abc",
+
+
+    get email(){
+        return this._email.toUpperCase();
+    },
+    set email(value){
+        this._email = value
+    }
+}
+
+const user1 = Object.create(User)
+console.log(user1.email);
