45ff11f4756d18283854851609313f9b66d92084
Sanu5584 <155883598+Sanu5584@users.noreply.github.com>
2024-05-04 05:52:05 +0000

    done

--- 10_classes_and_oops/11_getter_setter.js
+++ 10_classes_and_oops/11_getter_setter.js
@@ -1,1 +1,23 @@
-   
+class User{
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    }
+
+    get email(){
+        return this._email.toUpperCase();
+    }
+    set email(value){
+        this._email = value;
+    }
+    get password(){
+        return this._password.toUpperCase();
+    }
+    set password(value){
+        this._password = value;
+    }
+}    
+
+const user1 = new User("S@gmail.com", "abc");
+console.log(user1.email);
+console.log(user1.password);
